6425adac0a818a77e440f9136873475563eb896f
mehedi-hasan-anik <anikm2283@gmail.com>
2021-06-26 18:03:58 +0600

    editNavigation

--- src/components/Header/HeaderBottom/index.js
+++ src/components/Header/HeaderBottom/index.js
@@ -3,34 +3,9 @@ import "./HeaderBottom.css";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import navigationData from "../../../Data/navigaationData.json";
 
 const HeaderBottom = () => {
-  const fullNavigation = [
-    {
-      name: "Home",
-      id: "1",
-    },
-    {
-      name: "Category",
-      id: "2",
-    },
-    {
-      name: "Cart",
-      id: "3",
-    },
-    {
-      name: "Checkout",
-      id: "4",
-    },
-    {
-      name: "ProductDetails",
-      id: "5",
-    },
-    {
-      name: "LifeStyle",
-      id: "6",
-    },
-  ];
   return (
     <div className="full-nav-area">
       <nav className="navbar navbar-expand-lg   container">
@@ -56,7 +31,7 @@ const HeaderBottom = () => {
             id="navbarSupportedContent"
           >
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              {fullNavigation.map((event) => (
+              {navigationData.map((event) => (
                 <Link
                   className="nav-link ms-3"
                   key={event.id}
