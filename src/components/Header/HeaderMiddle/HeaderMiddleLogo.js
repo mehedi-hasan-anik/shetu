9f0d6efdb5e423b4f18b68fea7296f8bfd26c25e
mehedi-hasan-anik <anikm2283@gmail.com>
2021-06-26 19:07:31 +0600

    removeATag

--- src/components/Header/HeaderMiddle/HeaderMiddleLogo.js
+++ src/components/Header/HeaderMiddle/HeaderMiddleLogo.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderMiddlePhoto from "../../Image/logo.png";
+import { Link } from "react-router-dom";
 
 const HeaderMiddleLogo = () => {
   return (
     <div>
-      <a href="index.html">
+      <Link to="/">
+        {" "}
         <img src={HeaderMiddlePhoto} className="img-responsive" alt="" />
-      </a>
+      </Link>
     </div>
   );
 };
