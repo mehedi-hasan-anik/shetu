6f273ba9622154164a822eef55ae0b5f18ee7c94
mehedi-hasan-anik <anikm2283@gmail.com>
2021-06-27 10:38:45 +0600

    addClassName

--- src/components/Header/HeaderBottom/index.js
+++ src/components/Header/HeaderBottom/index.js
@@ -8,13 +8,13 @@ import navigationData from "../../../Data/navigaationData.json";
 const HeaderBottom = () => {
   return (
     <div className="full-nav-area">
-      <nav class="navbar navbar-expand-lg   container">
-        <div class="container-fluid nav-full-area">
-          <a class="navbar-brand" href="/">
+      <nav className="navbar navbar-expand-lg   container">
+        <div className="container-fluid nav-full-area">
+          <a className="navbar-brand" href="/">
             <h3>ALL Deparment</h3>
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -22,17 +22,17 @@ const HeaderBottom = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon">
+            <span className="navbar-toggler-icon">
               <FontAwesomeIcon icon={faBars} className="nav-bar-icon" />
             </span>
           </button>
           <div
-            class="collapse navbar-collapse navigation-area"
+            className="collapse navbar-collapse navigation-area"
             id="navbarSupportedContent"
           >
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               {navigationData.map((navigationitem) => (
-                <li class="nav-item" key={navigationitem.id}>
+                <li className="nav-item" key={navigationitem.id}>
                   <Link className="nav-link ms-3" to="/">
                     {navigationitem.name}
                   </Link>
