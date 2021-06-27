5c24f8ea5202dc2ecd40a5f8b6757a0c9582529e
mehedi-hasan-anik <anikm2283@gmail.com>
2021-06-27 14:12:30 +0600

    editPaginationBullet

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Footer.css";
-import About from './About'
+import About from "./About";
 import Communities from "./Communities";
 import Information from "./Information";
 import AppQrCode from "./AppQrCode";
@@ -10,29 +10,29 @@ const Footer = () => {
   return (
     <footer className="big-footer">
       <div className="container">
-        <div className="page-scroll back-top" data-section="/top">
+        {/* <div className="page-scroll back-top" data-section="/top">
           <i className="lnr lnr-chevron-up"></i>
-        </div>
+        </div> */}
         <div className="row">
           <div className="col-sm-4">
-            <About/>
+            <About />
           </div>
           <div className="col-sm-7 col-sm-offset-1 hidden-xs">
             <div className="row">
               <div className="col-md-4 hidden-sm">
-                <Communities/>
+                <Communities />
               </div>
               <div className="col-md-4 col-sm-6">
-                <Information/>
+                <Information />
               </div>
               <div className="col-md-4 col-sm-6">
-                <AppQrCode/>
+                <AppQrCode />
               </div>
             </div>
           </div>
         </div>
         <div className="app-download">
-          <DownloadTheApp/>
+          <DownloadTheApp />
         </div>
       </div>
     </footer>
